c43b3b27c2e9ab954c12726ad1b83bd21f671399
mrkadirov2005 <muzaffar571181@gmail.com>
2023-01-09 13:50:48 +0500

    in com

---
+++ main.js
@@ -0,0 +1,100 @@
+function changebackground(){
+    document.body.style.backgroundColor="#202c36";
+    document.getElementsByTagName("div")[0].style.backgroundColor="#202c36";
+    document.getElementsByTagName("div")[0].style.color="white";
+    document.getElementsByTagName("button")[0].style.backgroundColor="#2b3844";
+    document.getElementsByTagName("button")[0].style.color="white";
+    
+    document.getElementsByTagName("div")[3].style.backgroundColor="#2b3844";
+    
+    document.getElementsByTagName("div")[4].style.backgroundColor="#2b3844";
+    // document.getElementsByTagName("div")[4].style.color="white";
+    
+    document.getElementsByTagName("div")[4].classList.remove("animated");
+    
+    
+    
+    
+    
+    document.getElementsByTagName("div")[5].style.backgroundColor="#2b3844";
+    document.getElementsByTagName("div")[5].classList.remove("animated");
+    document.getElementsByTagName("div")[5].style.color="white";
+    
+    
+    // document.getElementsByTagName("div")[6].style.backgroundColor="#2b3844";
+    
+    document.forms[0].style.backgroundColor="#2b3844";
+    document.forms[0].style.color="white";
+    
+    document.forms[1].style.backgroundColor="#2b3844";
+    document.forms[0].style.color="red";
+    document.getElementsByTagName("div")[15].style.backgroundColor="#2b3844";
+    var card1=document.getElementById("card-1").style.backgroundColor="#2b3844"
+    var card2=document.getElementById("card-2").style.backgroundColor="#2b3844"
+    var card3=document.getElementById("card-3").style.backgroundColor="#2b3844"
+    var card4=document.getElementById("card-4").style.backgroundColor="#2b3844"
+    var card5=document.getElementById("card-5").style.backgroundColor="#2b3844"
+    var card6=document.getElementById("card-6").style.backgroundColor="#2b3844"
+    var card7=document.getElementById("card-7").style.backgroundColor="#2b3844"
+    var card8=document.getElementById("card-8").style.backgroundColor="#2b3844"
+    
+    var card1=document.getElementById("card-1").style.color="white"
+    var card2=document.getElementById("card-2").style.color="white"
+    var card3=document.getElementById("card-3").style.color="white"
+    var card4=document.getElementById("card-4").style.color="white"
+    var card5=document.getElementById("card-5").style.color="white"
+    var card6=document.getElementById("card-6").style.color="white"
+    var card7=document.getElementById("card-7").style.color="white"
+    var card8=document.getElementById("card-8").style.color="white"
+    
+    document.getElementById("selected-countries").style.backgroundColor="#2b3844";
+    document.getElementById("selected-countries").style.color="white";
+    
+    document.getElementById("button").style.display="none";
+    document.getElementById("buttonwhite").style.display="block";
+
+    
+}
+function changebackgroundwhite(){
+    document.body.style.backgroundColor="#f2f2f2";
+    var card1=document.getElementById("card-1").style.backgroundColor="white";
+    var card2=document.getElementById("card-2").style.backgroundColor="white";
+    var card3=document.getElementById("card-3").style.backgroundColor="white";
+    var card4=document.getElementById("card-4").style.backgroundColor="white";
+    var card5=document.getElementById("card-5").style.backgroundColor="white";
+    var card6=document.getElementById("card-6").style.backgroundColor="white";
+    var card7=document.getElementById("card-7").style.backgroundColor="white";
+    var card8=document.getElementById("card-8").style.backgroundColor="white";
+    
+    
+    var card1=document.getElementById("card-1").style.color="black"
+    var card2=document.getElementById("card-2").style.color="black"
+    var card3=document.getElementById("card-3").style.color="black"
+    var card4=document.getElementById("card-4").style.color="black"
+    var card5=document.getElementById("card-5").style.color="black"
+    var card6=document.getElementById("card-6").style.color="black"
+    var card7=document.getElementById("card-7").style.color="black"
+    var card8=document.getElementById("card-8").style.color="black"
+    document.getElementById("buttonwhite").style.display="block";
+    document.getElementById("header-top-part").style.backgroundColor="white";
+    document.getElementById("header-top-part").style.color="black";
+    
+    document.getElementById("header-bottom-part").style.backgroundColor="white";
+    document.getElementById("header-bottom-part").style.color="black";
+    document.getElementById("forms-of-header-bottom-left").style.backgroundColor="white";
+    document.getElementById("forms-of-header-bottom-left").style.color="black";
+    document.forms[0].style.backgroundColor="white";
+    
+    document.getElementById("forms-of-header-bottom-right").style.color="black";
+    document.forms[1].style.backgroundColor="white";
+    document.getElementById("selected-countries").style.backgroundColor="white";
+    document.getElementById("selected-countries").style.color="black";
+    document.getElementById("buttonwhite").style.display="none";
+
+    document.getElementById("button").style.display="block";
+    
+    
+    
+    
+    
+}
